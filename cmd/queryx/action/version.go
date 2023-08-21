5d77d682f3bb71b6ef9645fadf04d131bb2210bd
Wang Zuo <wzuoadjusted@gmail.com>
2023-08-21 16:56:47 +0800

    bump version

--- cmd/queryx/action/version.go
+++ cmd/queryx/action/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.2.2"
+var Version = "0.2.3"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ start() {
 
   pkg="github.com/swiftcarrot/queryx"
   bin="queryx.tar.gz"
-  version="v0.2.2"
+  version="v0.2.3"
 
   prefix=${PREFIX:-"/usr/local/bin"}
   tmp="$(mktmpdir)"
