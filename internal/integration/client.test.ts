4fc1990f5ebb32508a9e8160e494ebf0e95954cc
liangfujian <50279811+liangfujian@users.noreply.github.com>
2023-09-05 18:01:15 +0800

    fix gitaction

--- internal/integration/client.test.ts
+++ internal/integration/client.test.ts
@@ -349,18 +349,17 @@ test("AutomaticTransaction", async () => {
   let exists = await c.queryPost().where(c.postTitle.eq("AutomaticTransaction")).exists();
   expect(exists).toBe(false);
 
-  exists =await c.queryUser().where(c.userWeight.eq(190.99)).exists();
+  exists =await c.queryUser().where(c.userName.eq("user_name")).exists();
   expect(exists).toBe(false);
 
   await c.transaction(async function (tx: Tx) {
     let post = await tx.queryPost().create({title: "AutomaticTransaction"});
-    let user = await tx.queryUser().create({weight: 190.99});
-    console.log("user.weight....",user.weight)
+    let user = await tx.queryUser().create({name: "user_name"});
   } )
 
   exists = await c.queryPost().where(c.postTitle.eq("AutomaticTransaction")).exists();
   expect(exists).toBe(true);
-  exists = await c.queryUser().where(c.userWeight.eq(190.99)).exists();
+  exists = await c.queryUser().where(c.userName.eq("user_name")).exists();
   expect(exists).toBe(true);
 });
 
