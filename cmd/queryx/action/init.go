7f4a438b162f8ac5fb5f4422c7d6689ee6d6d88c
Zuo Wang <wzuoadjusted@gmail.com>
2024-10-04 13:50:59 +0800

    add `queryx init`

---
+++ cmd/queryx/action/init.go
@@ -0,0 +1,43 @@
+package action
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var sampleSchema = `database "db" {
+  adapter = "sqlite"
+
+  config "development" {
+    url = "sqlite:blog_development.sqlite3"
+  }
+
+  model "Post" {
+    column "title" {
+      type = string
+    }
+    column "content" {
+      type = text
+    }
+  }
+}
+`
+
+var initCmd = &cobra.Command{
+	Use: "init",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("Created schema.hcl")
+		file, err := os.Create("schema.hcl")
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = file.WriteString(sampleSchema)
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+}
