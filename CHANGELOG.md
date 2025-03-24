fe240fe878be207080c71d204deec9d06d612887
Zuo Wang <wzuoadjusted@gmail.com>
2025-03-25 00:40:58 +0800

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## Unreleased
 
+## 0.2.18
+
+- support string array in postgresql-golang
+
 ## 0.2.17
 
 - fix missing semicolon in migration sql files
--- cmd/queryx/action/version.go
+++ cmd/queryx/action/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.2.17"
+var Version = "0.2.18"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
